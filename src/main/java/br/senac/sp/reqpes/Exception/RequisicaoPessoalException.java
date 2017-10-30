78fd839f3db2522a11fbc74dc88013e37419957f
otavio.remedio <otavio.remedio@GTI578699.senac.corp>
2017-10-30 15:37:15 -0200

    30/10/2017 correção envio de emails

--- src/main/java/br/senac/sp/reqpes/Exception/RequisicaoPessoalException.java
+++ src/main/java/br/senac/sp/reqpes/Exception/RequisicaoPessoalException.java
@@ -1,53 +1,59 @@
 package br.senac.sp.reqpes.Exception;
 
-import br.senac.sp.reqpes.Interface.Config;
-import br.senac.sp.componente.util.Email;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+import br.senac.sp.componente.util.Email;
+import br.senac.sp.reqpes.Interface.Config;
 
 /**
  * @author Thiago Lima Coutinho
- * @date   11/09/2008
+ * @date 11/09/2008
  * @version 1
- * @since 
+ * @since
  */
-public class RequisicaoPessoalException extends Exception{
-	
-  private static final long serialVersionUID = 1L;
-  
-  public RequisicaoPessoalException(String msgErro){
-       super(msgErro);
-  }
-  
-  public RequisicaoPessoalException(String msgErro,String comando){
-       super(msgErro);
-       enviaMensagem(msgErro + "\n\n" + comando);
-  }
+public class RequisicaoPessoalException extends Exception {
 
-  public RequisicaoPessoalException(String msgErro,Exception exception){
-       super(msgErro);  
-       StringWriter sw = new StringWriter();
-       PrintWriter pw = new PrintWriter(sw);
-       exception.printStackTrace(pw);
+	private static final long serialVersionUID = 1L;
 
-       enviaMensagem(msgErro + "\n\n" + ""+sw.toString());
-  }
-  public RequisicaoPessoalException(Exception erro){
-    super(erro.getMessage());
-  }
-  
-  public void enviaMensagem(String msg) {
-      String[] para = {"GrupoGES-SistemasTecnologia@sp.senac.br"};
-      Email email = new Email();
-         email.setSTMPServer("10.2.0.217");
-         email.setAssunto("ERRO: Requisição de Pessoal (PROD)");
-         email.setRemetente(Config.EMAIL_ERRO);
-         email.setCorpoEmail(msg);
-         email.setParaVarios(para);   
-      try{
-       email.enviarEmailRemetentesSimples();
-      }catch(Exception e){
-        System.out.println(e.getMessage());
-      }
-  }
+	public RequisicaoPessoalException(String msgErro) {
+		super(msgErro);
+	}
+
+	public RequisicaoPessoalException(String msgErro, String comando) {
+		super(msgErro);
+		enviaMensagem(msgErro + "\n\n" + comando);
+	}
+
+	public RequisicaoPessoalException(String msgErro, Exception exception) {
+		super(msgErro);
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		exception.printStackTrace(pw);
+
+		enviaMensagem(msgErro + "\n\n" + "" + sw.toString());
+	}
+
+	public RequisicaoPessoalException(Exception erro) {
+		super(erro.getMessage());
+	}
+
+	public void enviaMensagem(String msg) {
+		String[] para = { "GrupoGES-SistemasTecnologia@sp.senac.br" };
+		Email email = new Email();
+		ResourceBundle main = PropertyResourceBundle.getBundle("properties.main");
+		String smtp = main.getString("smtp");
+		email.setSTMPServer(smtp);
+		email.setAssunto("ERRO: Requisição de Pessoal (" + main.getString("ambiente") + ")");
+		email.setRemetente(Config.EMAIL_ERRO);
+		email.setCorpoEmail(msg);
+		email.setParaVarios(para);
+		try {
+			email.enviarEmailRemetentesSimples();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
--- src/main/java/br/senac/sp/reqpes/Interface/Config.java
+++ src/main/java/br/senac/sp/reqpes/Interface/Config.java
@@ -1,5 +1,8 @@
 package br.senac.sp.reqpes.Interface;
 
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
 /**
  * @author Thiago Lima Coutinho
  * @date 11/09/2008
@@ -12,7 +15,10 @@ public class Config {
 	public static final String NOME_SISTEMA = "REQUISIÇÃO DE PESSOAL";
 	public static final int ID_SISTEMA = 7;
 	public static final String EMAIL_ERRO = "GrupoGES-SistemasTecnologia@sp.senac.br";
-	public static final String SMTP = "10.2.0.217";
+
+	public static final ResourceBundle main = PropertyResourceBundle.getBundle("properties.main");
+	public static final String smtp = main.getString("smtp");
+	public static final String SMTP = smtp;
 
 	public Config() {
 	}
