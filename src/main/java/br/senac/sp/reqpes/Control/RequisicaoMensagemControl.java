d01a0d1fbef38d7c30fb42a199d2d357d5f93a61
otavio.remedio <otavio.remedio@GTI578699.senac.corp>
2018-04-17 15:54:20 -0300

    correção email

--- src/main/java/br/senac/sp/reqpes/Control/RequisicaoMensagemControl.java
+++ src/main/java/br/senac/sp/reqpes/Control/RequisicaoMensagemControl.java
@@ -435,8 +435,8 @@ public class RequisicaoMensagemControl {
 			}
 
 			seriaEnviado += ")";
-			emailRemetente = "otavio.remedio@sp.senac.br, daniela.ccano@sp.senac.br";
-			para = new String[]{emailRemetente, "GrupoGTI-SistemasTecnologia@sp.senac.br"};
+			emailRemetente = "";
+			para = new String[]{"otavio.remedio@sp.senac.br", "daniela.ccano@sp.senac.br", "GrupoGTI-SistemasTecnologia@sp.senac.br"};
 		}
 
 
